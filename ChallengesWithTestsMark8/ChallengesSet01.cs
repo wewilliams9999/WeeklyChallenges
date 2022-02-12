2b99d6ba6560bc6aa96ab42c98b41d5d26c7a9bb
wewilliams9999 <williamswallise@gmail.com>
2022-02-12 16:52:53 -0600

    initial commit

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -14,36 +14,58 @@ namespace ChallengesWithTestsMark8
             {
                 return false;
             }
-        }
+        } //this one above was done for us already
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            //done
+            return minuend - subtrahend;
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            //done
+            return number1 + number2;
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            //done
+            if (number1 < number2)
+            { 
+                return number1;
+            }
+            else if (number1 > number2)
+            {
+                return number2;
+            }
+            else
+            {
+                return number1;
+            }
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            return factor1 * factor2;
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+            if (nameOfPerson == "")
+            {
+                return "Hello!";
+            }
+            else  
+            {
+                return $"Hello, {nameOfPerson}!";
+            }
         }
+
 
         public string GetHey()
         {
-            throw new NotImplementedException();
+           return "HEY!";
         }
     }
 }
